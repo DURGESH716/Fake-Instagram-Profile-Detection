b5484da058c6d55996ff7cf632818294d4948772
Durgesh Rao <77789927+DURGESH716@users.noreply.github.com>
2022-02-28 23:20:02 +0530

    Add files via upload

---
+++ Fake Instagram Detection_Using_ANN.py
@@ -0,0 +1,195 @@
+# Importing Libraries and Dataset
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import Accuracy
+
+from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report,accuracy_score,roc_curve,confusion_matrix
+
+# Load the training dataset
+instagram_df_train=pd.read_csv('insta_train.csv')
+instagram_df_train
+
+# Load the testing data
+instagram_df_test=pd.read_csv('insta_test.csv')
+instagram_df_test
+
+instagram_df_train.head()
+instagram_df_train.tail()
+
+instagram_df_test.head()
+instagram_df_test.tail()
+
+#Performing Exploratory Data Analysis EDA
+
+# Getting dataframe info
+instagram_df_train.info()
+
+# Get the statistical summary of the dataframe
+instagram_df_train.describe()
+
+# Checking if null values exist
+instagram_df_train.isnull().sum()
+
+# Get the number of unique values in the "profile pic" feature
+instagram_df_train['profile pic'].value_counts()
+
+# Get the number of unique values in "fake" (Target column)
+instagram_df_train['fake'].value_counts()
+
+instagram_df_test.info()
+
+instagram_df_test.describe()
+
+instagram_df_test.isnull().sum()
+
+instagram_df_test['fake'].value_counts()
+
+# Perform Data Visualizations
+
+# Visualize the data
+sns.countplot(instagram_df_train['fake'])
+plt.show()
+
+# Visualize the private column data
+sns.countplot(instagram_df_train['private'])
+plt.show()
+
+# Visualize the "profile pic" column data
+sns.countplot(instagram_df_train['profile pic'])
+plt.show()
+
+# Visualize the data
+plt.figure(figsize = (20, 10))
+sns.distplot(instagram_df_train['nums/length username'])
+plt.show()
+
+# Correlation plot
+plt.figure(figsize=(20, 20))
+cm = instagram_df_train.corr()
+ax = plt.subplot()
+sns.heatmap(cm, annot = True, ax = ax)
+plt.show()
+
+sns.countplot(instagram_df_test['fake'])
+
+sns.countplot(instagram_df_test['private'])
+
+sns.countplot(instagram_df_test['profile pic'])
+
+# Preparing Data to Train the Model
+
+# Training and testing dataset (inputs)
+X_train = instagram_df_train.drop(columns = ['fake'])
+X_test = instagram_df_test.drop(columns = ['fake'])
+X_train
+
+X_test
+
+# Training and testing dataset (Outputs)
+y_train = instagram_df_train['fake']
+y_test = instagram_df_test['fake']
+
+y_train
+
+y_test
+
+# Scale the data before training the model
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+scaler_x = StandardScaler()
+X_train = scaler_x.fit_transform(X_train)
+X_test = scaler_x.transform(X_test)
+
+y_train = tf.keras.utils.to_categorical(y_train, num_classes = 2)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes = 2)
+
+y_train
+
+y_test
+
+# print the shapes of training and testing datasets 
+X_train.shape, X_test.shape, y_train.shape, y_test.shape
+
+Training_data = len(X_train)/( len(X_test) + len(X_train) ) * 100
+Training_data
+
+Testing_data = len(X_test)/( len(X_test) + len(X_train) ) * 100
+Testing_data
+
+# Building and Training Deep Training Model
+
+import tensorflow.keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+
+model = Sequential()
+model.add(Dense(50, input_dim=11, activation='relu'))
+model.add(Dense(150, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(150, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(25, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(2,activation='softmax'))
+
+model.summary()
+
+model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+epochs_hist = model.fit(X_train, y_train, epochs = 50,  verbose = 1, validation_split = 0.1)
+
+import tensorflow.keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+
+model = Sequential()
+model.add(Dense(50, input_dim=11, activation='relu'))
+model.add(Dense(150, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(25, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(25, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(2, activation='softmax'))
+model.summary()
+
+# Access the Performance of the model
+
+print(epochs_hist.history.keys())
+
+plt.plot(epochs_hist.history['loss'])
+plt.plot(epochs_hist.history['val_loss'])
+
+plt.title('Model Loss Progression During Training/Validation')
+plt.ylabel('Training and Validation Losses')
+plt.xlabel('Epoch Number')
+plt.legend(['Training Loss', 'Validation Loss'])
+plt.show()
+
+predicted = model.predict(X_test)
+
+predicted_value = []
+test = []
+for i in predicted:
+    predicted_value.append(np.argmax(i))
+    
+for i in y_test:
+    test.append(np.argmax(i))
+
+print(classification_report(test, predicted_value))
+
+plt.figure(figsize=(10, 10))
+cm=confusion_matrix(test, predicted_value)
+sns.heatmap(cm, annot=True)
+plt.show()
+
+
